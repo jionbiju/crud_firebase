01d81335b2274d2c7bd2fda555cc24b1579f03f9
jionbiju <jionbiju@gmail.com>
2023-08-13 09:37:00 +0530

    simple home screen design created

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:crud_firebase/screens/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+      title:'Blood Donation Page',
+      theme: ThemeData(
+        primaryColor:Colors.red),
+        home: const HomeScreen(),
+    );
+  }
+}
